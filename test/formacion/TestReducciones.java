f0114901562ee2c8cc549236e54a1b1dedad901f
miguel alonso <migalonso@deloitte.com>
2023-09-30 11:43:37 -0600

    Problem to solve

---
+++ test/formacion/TestReducciones.java
@@ -0,0 +1,41 @@
+package formacion;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestReducciones {
+	
+	
+	private ReduccionesUtil util = new ReduccionesUtil();
+	
+	@Test
+	public void test_obteSinNulosYMayusculas() {
+		
+	    List<String> original = Arrays.asList("primero",null,"segundo");
+	    
+	    Collection<String> resultado = util.obtenSinNulosYMayusculas(original);
+	    
+	    assertEquals(2, resultado.size());
+	    assertTrue(resultado.contains("PRIMERO"));
+	    assertTrue(resultado.contains("SEGUNDO"));
+	    
+	}
+	
+	@Test
+	public void test_acumula_pares_impares() {
+		
+		List<Integer> lista = Arrays.asList(2, 3, 1, 5);
+		
+		int[] resultado = util.paresImpares(lista);
+		
+		assertEquals(1, resultado[0]);
+		assertEquals(3, resultado[1]);
+	}
+
+}
