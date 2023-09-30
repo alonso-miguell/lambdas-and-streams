72bfeeb30a1f35e944e5aa02ea431d23b6b825a4
miguel alonso <migalonso@deloitte.com>
2023-09-30 00:34:52 -0600

    Problem to solve

---
+++ src/formacion/AgrupadorConList.java
@@ -0,0 +1,12 @@
+package formacion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgrupadorConList implements Agrupador {
+
+	private List<Object> contenido = new ArrayList<>();
+	
+	// todo implementad los metodos de la interface usando la lista contenido
+
+}
